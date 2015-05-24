a217357ed85af319f1dc2dd2029076799534aaa3
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2015-05-24 17:50:31 +0500

    fixed

--- smtp.py
+++ smtp.py
@@ -1,14 +1,11 @@
 import base64
 import getpass
 from random import randint
-import os
 import socket
 import sys
 import time
 import ssl
-import quopri
-
-port = 465
+import json
 
 
 def recv_data(sock, timeout=2):
@@ -43,7 +40,6 @@ def print_help():
 
 
 def get_args():
-    # global login, password, recipient, host
     if len(sys.argv) < 2:
         print_help()
         sys.exit(0)
@@ -51,11 +47,10 @@ def get_args():
         host = sys.argv[1]
     login = input("LOGIN: ")
     password = getpass.getpass('PASS: ')
-    recipient = input("RECIPIENT: ")
-    return login, password, recipient, host
+    return login, password, host
 
 
-def gen_mess(login, recipient, all_files):
+def gen_mess(login, recipient, all_files, subject, text):
     delim = ''
     symb = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
             'abcdefghijklmnopqrstuvwxyz'
@@ -67,26 +62,24 @@ def gen_mess(login, recipient, all_files):
         delim += symb[randInd]
     result = ('FROM: {0} <{0}>\r\n'
               'TO: {1} <{1}>\r\n'
-              'Subject: test smtp\r\n'
+              'Subject: =?UTF-8?B?' + to_base64(subject) + '?=\r\n'
               'Content-Type: Multipart/mixed; boundary=\"{2}\"\r\n')
     result = result.format(login, recipient, delim)
     result += ('--' + delim + '\r\n'
                'Content-Type: text/plain; charset=utf-8\r\n'
                'Content-transfer-encoding: base64\r\n'
                '\r\n' +
-               to_base64('ну позязя')
+               to_base64(text)
                + '\r\n'
                )
     for f, content in all_files.items():
-        result += ('--{2}\r\n'
-                   'Content-Type: application/octet-stream; name=\"{4}\"\r\n'
+        result += ('--{0}\r\n'
+                   'Content-Type: application/octet-stream; name=\"{2}\"\r\n'
                    'Content-transfer-encoding: base64\r\n'
-                   'Content-Disposition: attachment; filename=\"{4}\"\r\n'
+                   'Content-Disposition: attachment; filename=\"{2}\"\r\n'
                    '\r\n'
-                   '{3}\r\n')
+                   '{1}\r\n')
         result = result.format(
-            login,
-            recipient,
             delim,
             content.decode('utf-8'),
             f
@@ -94,60 +87,68 @@ def gen_mess(login, recipient, all_files):
     return result
 
 
-def gen_dict_of_images():
+def gen_dict_of_files(attachments):
     all_files = {}
-    for f in os.listdir("./"):
-        ext = ['.jpg', '.png', '.jpeg']
-        is_image = filter(f.lower().endswith, ext)
-        if list(is_image):
-            with open(f, "rb") as image_file:
-                encoded_string = base64.b64encode(image_file.read())
-                all_files[f] = encoded_string
+    for f in attachments:
+        with open('./message/' + f, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+            all_files[f] = encoded_string
     return all_files
 
 
 def send(msg, sock):
     sock.send((msg + '\r\n').encode('utf-8'))
-    print(recv_data(sock))
+    recv_data(sock)
 
 
 def send_and_print(msg, sock):
-    print(msg)
     sock.send((msg + '\r\n').encode('utf-8'))
-    print(recv_data(sock))
+    recv_data(sock)
 
 
 def to_base64(s):
-    encoded_s = base64.b64encode(bytes(s, 'utf-8'))
+    encoded_s = base64.b64encode(s.encode('utf-8'))
     return encoded_s.decode('utf-8')
 
 
-def create_and_send_mess(sock, login, password, recipient, all_files):
-    print(recv_data(sock))
-    messages = [
-        'helo vintik9g',
-        'auth login',
-        to_base64('{0}'.format(login)),
-        to_base64('{0}'.format(password)),
-        'mail from: <{0}>'.format(login),
-        'rcpt to: <{0}>'.format(recipient),
-        'data'
-    ]
-    for m in messages:
-        send_and_print(m, sock)
-    send(gen_mess(login, recipient, all_files) + '\r\n.', sock)
+def create_and_send_mess(sock, login, password, recipients,
+                         all_files, subject, text):
+    for recipient in recipients:
+        recv_data(sock)
+        messages = [
+            'helo pieliedie',
+            'auth login',
+            to_base64('{0}'.format(login)),
+            to_base64('{0}'.format(password)),
+            'mail from: <{0}> AUTH={0}'.format(login),
+            'rcpt to: <{0}>'.format(recipient),
+            'data'
+        ]
+        for m in messages:
+            send_and_print(m, sock)
+        send(gen_mess(login, recipient, all_files,
+                      subject, text) + '\r\n.', sock)
 
 
 def main():
-    login, password, recipient, host = get_args()
+    port = 465
+    login, password, host = get_args()
     print(host + ' ' + repr(port))
     sock = ssl.SSLSocket(socket.socket())
     sock.connect((host, port))
+    with open('./message/config.conf', 'r', encoding='utf-8') as dump_f,\
+            open('./message/message.txt', 'r', encoding='utf-8') as msg_f:
+        dump = json.load(dump_f)
+        recipients = dump['recipients']
+        attachments = dump['attachments']
+        subject = dump['subject']
+        text = ''.join(msg_f.readlines())
 
-    all_files = gen_dict_of_images()
-    print("\nyou have {0} img files\n".format(len(all_files)))
+    all_files = gen_dict_of_files(attachments)
+    print("\nyou have {0} files\n".format(len(all_files)))
 
-    create_and_send_mess(sock, login, password, recipient, all_files)
+    create_and_send_mess(sock, login, password, recipients,
+                         all_files, subject, text)
 
 
 if __name__ == "__main__":
